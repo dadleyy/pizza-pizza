0e0b6762da2e630c5033d1aafc3bf79492531637
Danny Hadley <dadleyy@gmail.com>
2019-08-21 11:58:29 -0400

    first pass at change kind impl

--- Makefile
+++ Makefile
@@ -11,9 +11,10 @@ ARTIFACT=$(DIST)/artifacts/pizza-pizza.tar.gz
 APP_DIST=$(DIST)/$(NAME)
 SCRIPT=$(APP_DIST)/main.js
 INDEX=$(APP_DIST)/index.html
+STYLE=$(APP_DIST)/main.css
 
 HTML_FILES=$(shell git ls-files | grep ".html")
-STYLE_FILES=$(shell git ls-files | grep ".sass")
+STYLE_FILES=$(shell git ls-files | grep ".css")
 ELM_FILES=$(shell git ls-files | grep ".elm")
 ELM_MAIN=$(filter %Main.elm,$(ELM_FILES))
 
@@ -28,18 +29,25 @@ clean:
 	$(RM) $(STYLE)
 	$(RM) $(SCRIPT)
 	$(RM) $(ARTIFACT)
+	$(RM) $(APP_DIST)
 
 $(ARTIFACT): $(INDEX) $(SCRIPT) $(STYLE)
 	@echo "[pizza-pizza] creating artifact"
 	$(MKDIR) $(dir $(ARTIFACT))
 	tar -cvzf $(ARTIFACT) -C $(DIST) $(NAME)
 
-$(INDEX): $(HTML_FILES)
+$(APP_DIST):
+	$(MKDIR) $(APP_DIST)
+
+$(INDEX): $(HTML_FILES) $(APP_DIST)
 	@echo "[pizza-pizza] creating index.html [$(HTML_FILES)]"
 	$(COPY) $(HTML_FILES) $(INDEX)
 
-$(STYLE): $(STYLE_FILES)
+$(STYLE): $(STYLE_FILES) $(APP_DIST)
+	@echo "[pizza-pizza] creating main.css [$(STYLE_FILES)]"
+	$(COPY) $(filter %main.css, $(STYLE_FILES)) $(STYLE)
+	$(COPY) $(STYLE_FILES) $(APP_DIST)
 
-$(SCRIPT): $(ELM_FILES)
+$(SCRIPT): $(ELM_FILES) $(APP_DIST)
 	@echo "[pizza-pizza] compiling elm: $(ELM_FILES) [$(ELM_MAIN)]"
 	$(ELM) make $(ELM_MAIN) --output $(SCRIPT)
--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,17 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (section, article, button, text)
-import Html.Attributes exposing (class)
+import Html exposing (section, article, button, text, aside)
+import Html.Attributes exposing (class, disabled)
 import Html.Events exposing (onClick)
 
-type Message = AddItem Item
+type Message = AddItem Item | RemoveChange Int | Save | UndoChange Int
 
 type Item = Topping | Crust | Sauce
 
-type alias Model = { items : List Item }
+type Change = Addition Item | Removal Item | Modify Item (List Item) | NoChange Item
+
+type alias Model = { items : List Change }
 
 main : Program () Model Message
 main = Browser.document
@@ -28,10 +30,44 @@ init _ =
 
 
 -- VIEW
+itemDisplay : Item -> Html.Html Message
+itemDisplay item =
+  aside [ class "item-display" ] [
+    case item of
+      Crust ->
+        text "cust"
+      Topping ->
+        text "topping"
+      Sauce ->
+        text "sauce"
+  ]
 
-renderItem : Item -> Html.Html msg
-renderItem item =
-  article [] []
+renderItem : Int -> Change -> Html.Html Message
+renderItem index item =
+  case item of
+    Addition addition ->
+      article [ class "item-addition" ] [
+        itemDisplay addition,
+        aside [ class "item-controls" ] [
+          button [ onClick (RemoveChange index) ] [ text "remove" ]
+        ]
+      ]
+    Removal removal ->
+      article [ class "item-removal" ] [
+        itemDisplay removal,
+        aside [ class "item-controls" ] [
+          button [ onClick (UndoChange index) ] [ text "undo" ]
+        ]
+      ]
+    Modify current history ->
+      article [ class "item-modify" ] []
+    NoChange value ->
+      article [ class "item-no-change" ] [
+        itemDisplay value,
+        aside [ class "item-controls" ] [
+          button [ onClick (RemoveChange index) ] [ text "remove" ]
+        ]
+      ]
 
 view : Model ->  Browser.Document Message
 view model =
@@ -41,10 +77,11 @@ view model =
       section [ class "pizza-maker" ] [
         section [ class "pizza-maker-controls" ] [
           button [ onClick (AddItem Topping) ] [ text "add topping" ],
-          button [ ] [ text "add crust" ],
-          button [ ] [ text "add sauce" ]
+          button [ onClick (AddItem Crust), disabled (hasCrust model.items) ] [ text "add crust" ],
+          button [ onClick (AddItem Sauce) ] [ text "add sauce" ],
+          button [ onClick Save ] [ text "save" ]
         ],
-        section [ class "pizza-maker-view" ] (List.map renderItem model.items)
+        section [ class "pizza-maker-view" ] (List.indexedMap renderItem model.items)
       ]
     ]
   }
@@ -52,11 +89,79 @@ view model =
 
 -- UPDATE
 
+getCurrentvalue : Change -> Maybe Item
+getCurrentvalue change =
+  case change of
+    Addition addition -> Just addition
+    Removal _ -> Nothing
+    NoChange value -> Just value
+    Modify value history -> Just value
+
+isCrust : Maybe Item -> Bool
+isCrust maybe =
+  case maybe of
+    Nothing -> False
+    Just item ->
+      case item of
+        Crust -> True
+        Topping -> False
+        Sauce -> False
+
+hasCrust : List Change -> Bool
+hasCrust list =
+  List.any isCrust (List.map getCurrentvalue list)
+
+removeChange : Int -> List Change -> List Change
+removeChange index list =
+  case List.head (List.drop index list) of
+    Nothing ->
+      []
+    Just item ->
+      case item of
+        NoChange value ->
+          List.concat [(List.take index list), [Removal value], (List.drop (index + 1) list)]
+        Modify current history ->
+          list
+        Removal removal ->
+          list
+        Addition addition ->
+          List.concat [(List.take index list), (List.drop (index + 1) list)]
+
+undoChange : Int -> List Change -> List Change
+undoChange index list =
+  case List.head (List.drop index list) of
+    Nothing ->
+      []
+    Just item ->
+      case item of
+        NoChange value ->
+          List.concat [(List.take index list), [Removal value], (List.drop (index + 1) list)]
+        Modify current history ->
+          list
+        Removal removal ->
+          List.concat [(List.take index list), [NoChange removal], (List.drop (index + 1) list)]
+        Addition addition ->
+          List.concat [(List.take index list), (List.drop (index + 1) list)]
+
+toNoChange : Change -> Change
+toNoChange change =
+  case change of
+    NoChange value -> change
+    Modify current history -> NoChange current
+    Removal removal -> NoChange removal
+    Addition addition -> NoChange addition
+
 update : Message -> Model -> ( Model, Cmd msg )
 update msg model =
   case msg of
     AddItem item ->
-      ( { model | items = item :: model.items } , Cmd.none )
+      ( { model | items = (Addition item) :: model.items } , Cmd.none )
+    RemoveChange index ->
+      ( { model | items = removeChange index model.items } , Cmd.none )
+    UndoChange index ->
+      ( { model | items = undoChange index model.items } , Cmd.none )
+    Save ->
+      ( { model | items = List.map toNoChange model.items } , Cmd.none )
 
 -- Subscribe
 
